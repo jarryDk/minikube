55e02d8b35c79deee0555c69d57c90ad3bd28256
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2021-03-15 13:49:42 +0100

    Introduce secret

--- pgadmin/pgadmin-undeploy.sh
+++ pgadmin/pgadmin-undeploy.sh
@@ -2,6 +2,7 @@
 
 kubectl delete service pgadmin 
 kubectl delete deployment pgadmin
+kubectl delete secret pgadmin-secret
 kubectl delete configmap pgadmin-config
 kubectl delete persistentvolumeclaim pgadmin-pv-claim
 kubectl delete persistentvolume pgadmin-pv-volume
--- postgres/postgres-undeploy.sh
+++ postgres/postgres-undeploy.sh
@@ -2,6 +2,7 @@
 
 kubectl delete service postgres 
 kubectl delete deployment postgres
+kubectl delete secret postgres-secret
 kubectl delete configmap postgres-config
 kubectl delete persistentvolumeclaim postgres-pv-claim
 kubectl delete persistentvolume postgres-pv-volume
