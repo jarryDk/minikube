948cde240678917234cd45570da9decd095ebfcc
Michael Bornholdt Nielsen <michaelbornholdtnielsen@gmail.com>
2021-03-16 15:23:24 +0100

    Use statefulset for postgres

--- postgres/deploy-persistent.sh
+++ postgres/deploy-persistent.sh
@@ -4,7 +4,8 @@ kubectl apply -f postgres-configmap.yaml
 kubectl apply -f postgres-secret.yaml
 kubectl apply -f postgres-pv-volume.yaml
 kubectl apply -f postgres-pv-claim.yaml
-kubectl apply -f postgres-deployment-persistent.yaml
+kubectl apply -f postgres-statefulset-persistent.yaml
+# kubectl apply -f postgres-deployment-persistent.yaml
 kubectl apply -f postgres-service.yaml
 
 minikube service postgres --url
--- postgres/undeploy-persistent.sh
+++ postgres/undeploy-persistent.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 
 kubectl delete -f postgres-service.yaml
-kubectl delete -f postgres-deployment-persistent.yaml
+
+# kubectl delete -f postgres-deployment-persistent.yaml
+kubectl delete -f postgres-statefulset-persistent.yaml
+
 kubectl delete -f postgres-pv-claim.yaml
 kubectl delete -f postgres-pv-volume.yaml
 kubectl delete -f postgres-secret.yaml
